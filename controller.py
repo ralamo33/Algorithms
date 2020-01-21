462a863a75b4ae20fd9bff78b745173df1dbc417
ralamo33 <ralamo33@gmail.com>
2020-01-21 23:22:17 +0000

    Implemented controller run, which runs a search algorithm from the controller.

--- controller.py
+++ controller.py
@@ -6,25 +6,28 @@ import gui as view
 from enum import Enum
 import PIL
 
-class Algorithm(Enum):
-    """Enums representing the algorithm selected."""
-    BFS = "Breadth First Search"
-    DFS = "Depth First Search"
-
 class Controller:
     """Communicates between algorithms and gui."""
 
     def __init__(self, graph=model.MyGrid(), view=view.make_view()):
         self.model = graph
         self.view = view
-        self.algorithm = Algorithm.BFS
+        self.algorithm = model.Graph.bfs
         self.mutator = model.Status.NORMAL
 
     def mutate(self, x, y):
         """Mutate the tile of the given coordinates to the current mutator type."""
         self.model.mutate(model.Coordinate(x, y), self.mutator)
 
+    def run(self):
+        """Run an algorithm on the model."""
+        return self.algorithm(self.model)
+
 
 if __name__ == "__main__":
     c = Controller()
+    mine = model.MyGrid.bfs
+    grid = model.MyGrid()
+    answers = mine(grid)
+
 
--- controller_test.py
+++ controller_test.py
@@ -5,6 +5,25 @@ from gui import *
 
 
 class MyTestCase(unittest.TestCase):
+    v1 = Vertex("A")
+    v2 = Vertex("B")
+    v3 = Vertex("C")
+    v4 = Vertex("D")
+    v5 = Vertex("E")
+    verticies = [v1, v2, v3, v4, v5]
+    edges = [(v1, v2), (v2, v3), (v4, v5), (v1, v5)]
+    graph = Graph(verticies, edges)
+    controller = Controller(graph)
+
+    def test_run(self):
+        self.controller.algorithm = algorithms.Graph.bfs
+        self.assertEqual(self.controller.run(), [self.v1, self.v2, self.v5, self.v3, self.v4])
+        self.controller.algorithm = algorithms.Graph.dfs
+        self.assertEqual(self.controller.run(), [self.v1, self.v5, self.v4, self.v2, self.v3])
+        self.controller = Controller(self.graph)
+
+
+
     def test_mutate(self):
         c = Controller()
         c.mutator = Status.OBSTACLE
